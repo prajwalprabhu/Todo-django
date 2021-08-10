73dca79cee824e70d0984f032fa4f1748454f685
T Prajwal Prabhu <prajwalprabhu.tellar@gmail.com>
2021-08-10 22:30:49 +0530

    secret key

--- todo/settings.py
+++ todo/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m9tcogjfldh-db&s5s3@so0xq&&#431&)b+77heo+9e85hk53y'
+# SECRET_KEY = 'django-insecure-m9tcogjfldh-db&s5s3@so0xq&&#431&)b+77heo+9e85hk53y'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
